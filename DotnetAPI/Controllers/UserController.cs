fa8d67d784a7bc98a068c7e8b66bbacad5c57e0b
cpkopp <cameronpkopp@gmail.com>
2023-09-29 17:51:14 -1000

    connect to docker sql container

--- DotnetAPI/Controllers/UserController.cs
+++ DotnetAPI/Controllers/UserController.cs
@@ -7,10 +7,20 @@ namespace DotnetAPI.Controllers;
 public class UserController : ControllerBase //inhereting ControllerBase class
 {
 
-    public UserController()
+    DataContextDapper _dapper;
+
+    public UserController(IConfiguration config)
     {
-        
+        _dapper = new DataContextDapper(config);
     }
+
+    [HttpGet("TestConnection")]
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
+
+
 
     [HttpGet("GetUsers/{testValue}")]
 
