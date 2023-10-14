7fea5caaa4fb3599de7d3f35a548e341205e91b9
cpkopp <cameronpkopp@gmail.com>
2023-10-13 14:24:06 -1000

    posts controller

--- DotnetAPI/Dtos/PostToEditDto.cs
+++ DotnetAPI/Dtos/PostToEditDto.cs
@@ -1,15 +1,12 @@
-namespace DotnetAPI.Models
+namespace DotnetAPI.Dtos
 {
-    public partial class Posts
+    public partial class PostToEditDto
     {
         public int PostId {get; set;}
-        public int UserId {get; set;}
         public string PostTitle {get; set;}
         public string PostContent {get; set;}
-        public DateTime PostCreated {get; set;}
-        public DateTime PostUpdated {get; set;}
 
-        public Posts()
+        public PostToEditDto()
         {
             if (PostTitle == null)
             {
